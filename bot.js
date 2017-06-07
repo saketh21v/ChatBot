1bf8dd0656955cf892811516ee2d046eca9740d1
Vallakatla <Saketh_Vallakatla@Dell.com>
2017-06-08 00:01:49 +0530

    Timeout Implemented

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ var Wit = require('node-wit').Wit; // Wit.ai node SDK
 var Constants = require('./lib/Constants.js');
 var CONV_DB = Constants.CONV_DB;
 var ID_DB = Constants.ID_DB
-
+var ID_Status = Constants.ID_Status;
 var _ENDPOINT = Constants._ENDPOINT;
 
 /*----------------------------------------------------------------------------------------------------*/
@@ -18,12 +18,12 @@ var _ENDPOINT = Constants._ENDPOINT;
 // Webserver parameter setup
 const PORT = process.env.PORT || 3000
 const app = express();
-const client = new Wit({ accessToken: Constants._TOKEN}); // Wit.ai client
+const client = new Wit({ accessToken: Constants._TOKEN }); // Wit.ai client
 
 /*----------------------------------------------------------------------------------------------------*/
 
 // Initial setup
-ID_DB.push(0); // Default ID for test purpopses
+ID_DB.addID(ID_Status(0)); // Default ID for test purpopses
 
 /*----------------------------------------------------------------------------------------------------*/
 
@@ -31,7 +31,7 @@ ID_DB.push(0); // Default ID for test purpopses
 
 /* Function to setup and initialize 
 conversation parameters and records in CONV_DB */
-function setupConv(id) { 
+function setupConv(id) {
     CONV_DB[id] = {
         numMsgs: 0,
         product: {
@@ -47,19 +47,24 @@ function setupConv(id) {
 // ID generator: returns new ID
 function generateId() {
     var id = Math.round(Math.random() * 1000000000);
-    while (ID_DB.indexOf(id) != -1) id = Math.round(Math.random() * 1000000000);
-    ID_DB.push(id);
+    while (ID_DB.exists(id)) {
+        id = Math.round(Math.random() * 1000000000);
+        console.log('Trying ID = ' + id);
+    }
+    var id_s = ID_Status(id);
+    ID_DB.addID(id_s);
     return id;
 }
 
 /*----------------------------------------------------------------------------------------------------*/
 
 // Routes and Endpoint functions
-app.get(_ENDPOINT + 'id', function(req, res){
+app.get(_ENDPOINT + 'id', function (req, res) {
+    console.log('Received ID request');
     var id = generateId();
     setupConv(id);
-    res.end(JSON.stringify({id: id}));
-    console.log('ID Request addressed. Sent ID : '+ JSON.stringify({id: id}));
+    res.end(JSON.stringify({ id: id }));
+    console.log('ID Request addressed. Sent ID : ' + JSON.stringify({ id: id }));
 });
 
 /*----------------------------------------------------------------------------------------------------*/
